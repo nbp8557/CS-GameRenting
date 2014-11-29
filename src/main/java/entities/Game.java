59d24ca687dd59439f4f5dc5bc7735da1e6a6ff1
Nathan Perry <nbp8557@rit.edu>
2014-11-29 16:06:21 -0500

    Updated entities

---
+++ src/main/java/entities/Game.java
@@ -0,0 +1,91 @@
+package entities;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.List;
+
+
+/**
+ * The persistent class for the Game database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Game.findAll", query="SELECT g FROM Game g")
+public class Game implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int gameID;
+
+	private String name;
+
+	private byte rentable;
+
+	//bi-directional many-to-one association to Console
+	@ManyToOne
+	@JoinColumn(name="ConsoleName")
+	private Console console;
+
+	//bi-directional many-to-one association to Rental
+	@OneToMany(mappedBy="game")
+	private List<Rental> rentals;
+
+	public Game() {
+	}
+
+	public int getGameID() {
+		return this.gameID;
+	}
+
+	public void setGameID(int gameID) {
+		this.gameID = gameID;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public byte getRentable() {
+		return this.rentable;
+	}
+
+	public void setRentable(byte rentable) {
+		this.rentable = rentable;
+	}
+
+	public Console getConsole() {
+		return this.console;
+	}
+
+	public void setConsole(Console console) {
+		this.console = console;
+	}
+
+	public List<Rental> getRentals() {
+		return this.rentals;
+	}
+
+	public void setRentals(List<Rental> rentals) {
+		this.rentals = rentals;
+	}
+
+	public Rental addRental(Rental rental) {
+		getRentals().add(rental);
+		rental.setGame(this);
+
+		return rental;
+	}
+
+	public Rental removeRental(Rental rental) {
+		getRentals().remove(rental);
+		rental.setGame(null);
+
+		return rental;
+	}
+
+}
---
+++ src/main/java/entities/Rental.java
@@ -0,0 +1,81 @@
+package entities;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.Date;
+
+
+/**
+ * The persistent class for the Rental database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Rental.findAll", query="SELECT r FROM Rental r")
+public class Rental implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Temporal(TemporalType.DATE)
+	private Date rentalTimestamp;
+
+	@Temporal(TemporalType.DATE)
+	private Date returnTimestamp;
+
+	//bi-directional many-to-one association to Person
+	@ManyToOne
+	@JoinColumn(name="EboardID")
+	private Person person1;
+
+	//bi-directional many-to-one association to Game
+	@ManyToOne
+	@JoinColumn(name="GameID")
+	private Game game;
+
+	//bi-directional many-to-one association to Person
+	@ManyToOne
+	@JoinColumn(name="RentorID")
+	private Person person2;
+
+	public Rental() {
+	}
+
+	public Date getRentalTimestamp() {
+		return this.rentalTimestamp;
+	}
+
+	public void setRentalTimestamp(Date rentalTimestamp) {
+		this.rentalTimestamp = rentalTimestamp;
+	}
+
+	public Date getReturnTimestamp() {
+		return this.returnTimestamp;
+	}
+
+	public void setReturnTimestamp(Date returnTimestamp) {
+		this.returnTimestamp = returnTimestamp;
+	}
+
+	public Person getPerson1() {
+		return this.person1;
+	}
+
+	public void setPerson1(Person person1) {
+		this.person1 = person1;
+	}
+
+	public Game getGame() {
+		return this.game;
+	}
+
+	public void setGame(Game game) {
+		this.game = game;
+	}
+
+	public Person getPerson2() {
+		return this.person2;
+	}
+
+	public void setPerson2(Person person2) {
+		this.person2 = person2;
+	}
+
+}
